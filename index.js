5536739af01fe71ba3949ab4f40a7e733b6c5c48
Carly Pecora <carlyannpecora@gmail.com>
2019-03-25 16:38:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key, value){
   return object
 }
 
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object)
+}
